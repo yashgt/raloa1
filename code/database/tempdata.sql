f765033f0862c616def1ea6f64a3be2705dffdbe
yashgt <yashgt@d4ffbb97-68be-d264-9e52-e6dc06afde82>
2014-10-11 14:25:15 +0000

    db changes

--- code/database/tempdata.sql
+++ code/database/tempdata.sql
@@ -1,2 +1,13 @@
-insert into fleet(fleet_name) values('Goa');
+insert into fleet(parent_fleet_id, fleet_name) values(null, 'No Fleet');
+select * from fleet;
 insert into user(username, password, fleet_id, role_type) values ('yash', 'yash123', 1, 2);
+
+insert into fleet(fleet_name, parent_fleet_id, fleet_type, avg_speed) values ('Goa Transport', 1, 3, 30);
+set @goatransid = last_insert_id();
+insert into fleet(fleet_name, parent_fleet_id, fleet_type, avg_speed) values ('KTCL', @goatransid, 3, 30);
+set @ktcltransid = last_insert_id();
+insert into fleet(fleet_name, parent_fleet_id, fleet_type, avg_speed) values ('KTCL Shuttles', @ktcltransid, 3, 30);
+insert into fleet(fleet_name, parent_fleet_id, fleet_type, avg_speed) values ('KTCL interstate', @ktcltransid, 3, 30);
+insert into fleet(fleet_name, parent_fleet_id, fleet_type, avg_speed) values ('Private Buses', @goatransid, 3, 30);
+
+insert into fleet(fleet_name, parent_fleet_id, fleet_type, avg_speed) values ('MH Transport', 1, 3, 30);
