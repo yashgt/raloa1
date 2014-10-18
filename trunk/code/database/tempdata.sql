d77caffab93face3bbcd0b23f5c472a19cb6195b
yashgt <yashgt@d4ffbb97-68be-d264-9e52-e6dc06afde82>
2014-10-18 11:12:28 +0000

    data changes

--- trunk/code/database/tempdata.sql
+++ trunk/code/database/tempdata.sql
@@ -2,7 +2,9 @@ insert into fleet(parent_fleet_id, fleet_name) values(null, 'No Fleet');
 insert into user(username, password, fleet_id, role_type) values ('yash', 'yash123', 1, 2);
 
 
-insert into fleet(fleet_name, parent_fleet_id, fleet_type, avg_speed) values ('Goa Transport', 1, 3, 30);
+insert into fleet(fleet_name, parent_fleet_id, fleet_type, avg_speed,cen_lat,cen_lon,ne_lat,ne_lon,sw_lat,sw_lon, zoom) 
+values ('Goa Transport', 1, 3, 30, 15.4989, 73.8278, 15.855126, 74.421425, 14.867264, 73.622169, 12);
+
 set @goatransid = last_insert_id();
 insert into fleet(fleet_name, parent_fleet_id, fleet_type, avg_speed) values ('KTCL', @goatransid, 3, 30);
 set @ktcltransid = last_insert_id();
