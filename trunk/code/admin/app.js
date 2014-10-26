79f442d3d5e52a843d6e01163105c2442b84285b
yashgt <yashgt@d4ffbb97-68be-d264-9e52-e6dc06afde82>
2014-10-26 17:24:47 +0000

    route search added

--- trunk/code/admin/app.js
+++ trunk/code/admin/app.js
@@ -91,28 +91,28 @@ http.createServer(app).listen(app.get('port'), function() {
 app.get('/api/fleets', function(req, res) {
     var user_id = req.session.passport.user.userId;
     db.query("call list_user_fleets(?);", [user_id], function(results) {
-            res.json(results[0].map(
-                function(fleet) {
-                    return {
-                        fleet_id: fleet.fleet_id,
-                        fleet_name: fleet.fleet_name,
-                        level: fleet.level
-                    };
-                }));
-				});
+        res.json(results[0].map(
+            function(fleet) {
+                return {
+                    fleet_id: fleet.fleet_id,
+                    fleet_name: fleet.fleet_name,
+                    level: fleet.level
+                };
+            }));
+    });
 });
 
 app.post('/api/stop', function(req, res) {
     var stopDetail = req.body;
     stopDetail.fleetId = req.session.passport.user.rootFleetId;
     logger.debug("Saving stop {0}", stopDetail);
-    db.query("set @id := ? ; call save_stop(@id,?,?,?,?) ; select @id; ", [stopDetail.id, stopDetail.name, stopDetail.latitude, stopDetail.longitude, stopDetail.fleetId], function( results) {
-            var id = results[2][0]["@id"];
-            console.log("Stop created with ID : %j", id);
-            logger.info("Name of the stop : ", stopDetail.name);
-            res.json({
-                id: id
-            });
+    db.query("set @id := ? ; call save_stop(@id,?,?,?,?) ; select @id; ", [stopDetail.id, stopDetail.name, stopDetail.latitude, stopDetail.longitude, stopDetail.fleetId], function(results) {
+        var id = results[2][0]["@id"];
+        console.log("Stop created with ID : %j", id);
+        logger.info("Name of the stop : ", stopDetail.name);
+        res.json({
+            id: id
+        });
     });
 });
 
@@ -124,57 +124,58 @@ app.post('/api/currentFleet', function(req, res) {
 
 app.post('/api/calendar', function(req, res) {
     var calendar = req.body;
-	logger.debug("Saving calendar: ", calendar);
-	//db.query
-    
-    res.json({calendarId:1});
+    logger.debug("Saving calendar: ", calendar);
+    //db.query
+
+    res.json({
+        calendarId: 1
+    });
 });
 
 app.get('/api/fleet/:fleet_id', function(req, res) {
     var fleetId = req.params.fleet_id;
-	db.query("call get_fleet_detail(?);", [fleetId], function(results){
-		var fleetDetail = {
-                center: {
-                    latitude: results[0][0].cen_lat,
-                    longitude: results[0][0].cen_lon
+    db.query("call get_fleet_detail(?);", [fleetId], function(results) {
+        var fleetDetail = {
+            center: {
+                latitude: results[0][0].cen_lat,
+                longitude: results[0][0].cen_lon
+            },
+            zoom: results[0][0].zoom,
+            bounds: {
+                northeast: {
+                    latitude: results[0][0].ne_lat,
+                    longitude: results[0][0].ne_lon
                 },
-                zoom: results[0][0].zoom,
-                bounds: {
-                    northeast: {
-                        latitude: results[0][0].ne_lat,
-                        longitude: results[0][0].ne_lon
-                    },
-                    southwest: {
-                        latitude: results[0][0].sw_lat,
-                        longitude: results[0][0].sw_lon
-                    }
-                },
-                stops: results[1].map(function(stop) {
-                    return {
-                        id: stop.stop_id,
-                        latitude: stop.latitude,
-                        longitude: stop.longitude,
-                        name: stop.name
-                    };
-                }),
-				calendars: [
-					{serviceName : 'All days'
-					, mon : true
-					, tue : true
-					, wed : true
-					, thu : true
-					, fri : true
-					, sat : true
-					, sun : true
-					, startDate: '2014-10-1'
-					, endDate: '2100-10-1'
-					}					
-				],
-                routes: [] //TODO
-            };
-            res.json(fleetDetail);
-	});
-	/*
+                southwest: {
+                    latitude: results[0][0].sw_lat,
+                    longitude: results[0][0].sw_lon
+                }
+            },
+            stops: results[1].map(function(stop) {
+                return {
+                    id: stop.stop_id,
+                    latitude: stop.latitude,
+                    longitude: stop.longitude,
+                    name: stop.name
+                };
+            }),
+            calendars: [{
+                serviceName: 'All days',
+                mon: true,
+                tue: true,
+                wed: true,
+                thu: true,
+                fri: true,
+                sat: true,
+                sun: true,
+                startDate: '2014-10-1',
+                endDate: '2100-10-1'
+            }],
+            routes: [] //TODO
+        };
+        res.json(fleetDetail);
+    });
+    /*
     dbConn.query("call get_fleet_detail(?);", [fleetId], function(err, results) {
         if (err == undefined) {
             console.log("Results %j ", results);
@@ -223,34 +224,40 @@ app.get('/api/fleets/:fleetgroup_id', function(req, res) {
     });
 });
 
-app.get('/api/route/:route_id' , function(req, res) {
-	//TODO get from DB
-	res.json({
-		routeId: 1
-		,stops: [ {id:100, name:'Panjim'}, {id:101, name:'Porvorim'}, {id:102, name:'Mapusa'}]
-		,timings: [
-			{
-				tripId: 1
-				,direction : 0
-				,frequency_trip: true
-				,frequency_start_time: '09:00'
-				,frequency_end_time: '10:00'
-				,'100' : '09:00'
-				,'101' : '09:10'
-				,'102' : '09:20'
-			}
-			,{
-				tripId: 2
-				,direction : 0
-				,frequency_trip: true
-				,frequency_start_time: '09:00'
-				,frequency_end_time: '10:00'
-				,'100' : '09:00'
-				,'101' : '09:10'
-				,'102' : '09:20'
-			}
-			]		
-	});
+app.get('/api/route/:route_id', function(req, res) {
+    //TODO get from DB
+    res.json({
+        routeId: 1,
+        stops: [{
+            id: 100,
+            name: 'Panjim'
+        }, {
+            id: 101,
+            name: 'Porvorim'
+        }, {
+            id: 102,
+            name: 'Mapusa'
+        }],
+        timings: [{
+            tripId: 1,
+            direction: 0,
+            frequency_trip: true,
+            frequency_start_time: '09:00',
+            frequency_end_time: '10:00',
+            '100': '09:00',
+            '101': '09:10',
+            '102': '09:20'
+        }, {
+            tripId: 2,
+            direction: 0,
+            frequency_trip: true,
+            frequency_start_time: '09:00',
+            frequency_end_time: '10:00',
+            '100': '09:00',
+            '101': '09:10',
+            '102': '09:20'
+        }]
+    });
 });
 
 //AUTH REGION
