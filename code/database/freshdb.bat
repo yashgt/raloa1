3da773dff2384ba44456700e450337b2228933df
yashgt <yashgt@d4ffbb97-68be-d264-9e52-e6dc06afde82>
2014-12-20 13:54:23 +0000

    major chnages

--- code/database/freshdb.bat
+++ code/database/freshdb.bat
@@ -1,2 +1,3 @@
 mysql --user=root --password=goatrans < tables.sql
 mysql --user=root --password=goatrans --database=avishkar < procs.sql
+mysql --user=root --password=goatrans --database=avishkar < tempdata.sql
