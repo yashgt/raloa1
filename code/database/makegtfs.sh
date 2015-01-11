a760e109281d841f87cf71ab9b84a44f32abd3e8
yashgt <yashgt@d4ffbb97-68be-d264-9e52-e6dc06afde82>
2015-01-11 06:54:56 +0000

    GTFS code

--- code/database/makegtfs.sh
+++ code/database/makegtfs.sh
@@ -5,7 +5,6 @@ mkdir -p ${folder}
 
 export mypath="C:\Program Files\MySQL\MySQL Server 5.6\bin"
 
-cd ${folder}
 myopts="-uroot -pgoatrans --database=avishkar -A"
 
-"${mypath}/mysql" ${myopts} -e"set @fleet_id=${fleet_id}; source ../agency.sql;" | tr '\t' ',' > agency.txt
+"${mypath}/mysql" ${myopts} -e"set @fleet_id=${fleet_id}; source agency.sql;" | tr '\t' ',' > ${folder}\agency.txt
