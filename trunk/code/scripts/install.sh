4ad9bfff7b803522f3799b33ee6e3a034b1d4e54
yashgt@gmail.com <yashgt@gmail.com@d4ffbb97-68be-d264-9e52-e6dc06afde82>
2015-02-05 14:09:49 +0000

    scripts

---
+++ trunk/code/scripts/install.sh
@@ -0,0 +1,15 @@
+sudo apt-get install g++ curl libssl-dev apache2-utils
+sudo apt-get install git-core
+git clone https://github.com/joyent/node.git
+sudo apt-get install mysql-server-core-5.5
+sudo apt-get install mysql-client-5.5
+sudo apt-get install ruby
+sudo gem install bundler
+sudo gem install capistrano
+sudo gem install capistrano-node-deploy
+sudo npm install -g bower
+sudo npm install minify -g
+wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.6.zip
+sudo apt-get install unzip
+unzip google_appengine_1.9.6.zip
+chmod 755 google_appengine/appcfg.py
