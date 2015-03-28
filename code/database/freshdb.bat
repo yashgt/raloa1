74b8261edb4b52486a557029e24f40a71a0caa57
newyugtech@gmail.com <newyugtech@gmail.com@d4ffbb97-68be-d264-9e52-e6dc06afde82>
2015-03-28 14:29:38 +0000

    server changes

--- code/database/freshdb.bat
+++ code/database/freshdb.bat
@@ -1,3 +1,7 @@
-mysql --user=root --password=goatrans < tables.sql
-mysql --user=root --password=goatrans --database=avishkar < procs.sql
-mysql --user=root --password=goatrans --database=avishkar < tempdata.sql
+host=ny-prod-db.cvoo5nr5votw.ap-southeast-1.rds.amazonaws.com
+user=root
+pwd=goatransport
+mysql --host=${host} --user=${user} --password=${pwd} < tables.sql
+mysql --host=${host} --user=${user} --password=${pwd} --database=avishkar < procs.sql
+mysql --host=${host} --user=${user} --password=${pwd} --database=avishkar < tempdata.sql
+
--- code/scripts/install.sh
+++ code/scripts/install.sh
@@ -1,15 +1,22 @@
-sudo apt-get install g++ curl libssl-dev apache2-utils
-sudo apt-get install git-core
-git clone https://github.com/joyent/node.git
-sudo apt-get install mysql-server-core-5.5
-sudo apt-get install mysql-client-5.5
-sudo apt-get install ruby
+sudo yum update
+
+sudo yum install g++ curl libssl-dev apache2-utils
+sudo yum install git-core
+
+sudo yum install wget
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+sudo rpm -ivh epel-release-7-5.noarch.rpm
+
+#git clone https://github.com/joyent/node.git
+#sudo yum install mysql-server-core-5.5
+sudo yum install mysql
+sudo yum install ruby
 sudo gem install bundler
 sudo gem install capistrano
 sudo gem install capistrano-node-deploy
-sudo npm install -g bower
+sudo npm install bower -g
 sudo npm install minify -g
-wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.6.zip
-sudo apt-get install unzip
-unzip google_appengine_1.9.6.zip
-chmod 755 google_appengine/appcfg.py
+#wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.6.zip
+sudo yum install unzip
+#unzip google_appengine_1.9.6.zip
+#chmod 755 google_appengine/appcfg.py
