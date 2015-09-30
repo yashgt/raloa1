29b03c10f4c6a1d7487d18d1835b231622a6d811
Cloud User <ec2-user@ip-172-31-20-22.ap-southeast-1.compute.internal>
2015-09-30 18:56:55 +0530

    jq used to read from config

--- code/database/makegtfs.sh
+++ code/database/makegtfs.sh
@@ -3,13 +3,15 @@ echo $fleet_id
 folder=gtfs_$fleet_id
 mkdir -p ${folder}
 
-export mypath="C:\Program Files\MySQL\MySQL Server 5.6\bin"
+#export mypath="C:\Program Files\MySQL\MySQL Server 5.6\bin"
+host=`jq -r ".database.host" ../config.json`
 
-myopts="-uroot -pgoatrans --database=avishkar -A"
+myopts="-uroot -pgoatransport --database=raloa2 --host=${host}"
+echo ${myopts}
 
-"${mypath}/mysql" ${myopts} -e"set @fleet_id=${fleet_id}; source agency.sql;" | tr '\t' ',' > ${folder}\\agency.txt
-"${mypath}/mysql" ${myopts} -e"set @fleet_id=${fleet_id}; source route.sql;" | tr '\t' ',' > ${folder}\\route.txt
-"${mypath}/mysql" ${myopts} -e"set @fleet_id=${fleet_id}; source stop.sql;" | tr '\t' ',' > ${folder}\\stop.txt
-"${mypath}/mysql" ${myopts} -e"set @fleet_id=${fleet_id}; source trip.sql;" | tr '\t' ',' > ${folder}\\trip.txt
-"${mypath}/mysql" ${myopts} -e"set @fleet_id=${fleet_id}; source frequencies.sql;" | tr '\t' ',' > ${folder}\\frequencies.txt
-"${mypath}/mysql" ${myopts} -e"set @fleet_id=${fleet_id}; source stop_times.sql;" | tr '\t' ',' > ${folder}\\stop_times.txt
+mysql ${myopts} -e"set @fleet_id=${fleet_id}; source agency.sql;" | tr '\t' ',' > ${folder}/agency.txt
+mysql ${myopts} -e"set @fleet_id=${fleet_id}; source route.sql;" | tr '\t' ',' > ${folder}/route.txt
+mysql ${myopts} -e"set @fleet_id=${fleet_id}; source stop.sql;" | tr '\t' ',' > ${folder}/stop.txt
+mysql ${myopts} -e"set @fleet_id=${fleet_id}; source trip.sql;" | tr '\t' ',' > ${folder}/trip.txt
+mysql ${myopts} -e"set @fleet_id=${fleet_id}; source frequencies.sql;" | tr '\t' ',' > ${folder}/frequencies.txt
+mysql ${myopts} -e"set @fleet_id=${fleet_id}; source stop_times.sql;" | tr '\t' ',' > ${folder}/stop_times.txt
