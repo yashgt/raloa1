b62b40e81f04bdefcf40ee1b14ee89c9a517c4d0
Cloud User <ec2-user@ip-172-31-20-22.ap-southeast-1.compute.internal>
2015-10-24 16:20:22 +0530

    duplicate stops identifier

--- code/scripts/dups.sql
+++ code/scripts/dups.sql
@@ -1,1 +1,13 @@
-select peer_stop_id from stop group by peer_stop_id having count(*)>1;
+select S.stop_id, S.name, S.peer_stop_id, R.route_id
+from stop S
+left outer join routestop RS on (S.stop_id=RS.stop_id)
+left outer join route R on (RS.route_id=R.route_id)
+inner join (
+select 
+peer_stop_id 
+from stop group by peer_stop_id
+having count(*)>1 and peer_stop_id is not null
+) D on (S.peer_stop_id=D.peer_stop_id)
+
+
+order by S.peer_stop_id;
