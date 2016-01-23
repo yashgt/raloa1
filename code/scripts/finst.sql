dfa5680ee44468864c8e971d570297b72cde212b
unknown <yashgt@gmail.com>
2016-01-23 12:41:21 +0530

    fix stop names

--- code/scripts/finst.sql
+++ code/scripts/finst.sql
@@ -1,9 +1,23 @@
 select
-name
-,substr(name from 1 for instr(lower(name), ' bus stop')-1)
-,substr(name from 1 for instr(lower(name), '  bus  stop')-1)
+stop_id
+,name
+,quote(substr(name from 1 for instr(lower(name), ' bus stop')-1)) as op1
+/*,substr(name from 1 for instr(lower(name), '  bus  stop')-1) as op2*/
 from stop
 WHERE 
-lower(name) REGEXP '[[:space:]]*bus[[:space:]]*stop'
-and stop_id=1230
+lower(name) like '% bus stop%'
+/*and stop_id=1230*/
 ;
+
+select
+stop_id
+,name
+/*,substr(name from 1 for instr(lower(name), ' bus stop')-1) as op1*/
+,quote(substr(name from 1 for instr(lower(name), '  bus  stop')-1)) as op2
+from stop
+WHERE 
+lower(name) like '%  bus  stop%'
+/*and stop_id=1230*/
+;
+
+
--- code/scripts/fixnames.sql
+++ code/scripts/fixnames.sql
@@ -2,12 +2,23 @@ UPDATE stop
 SET 
 name = substr(name from 1 for instr(lower(name), ' bus stop')-1)
 WHERE 
-lower(name) REGEXP '[[:space:]]*bus[[:space:]]*stop'
+lower(name) like '% bus stop%'
 ;
 
 UPDATE stop
 SET 
 name = substr(name from 1 for instr(lower(name), '  bus  stop')-1)
 WHERE 
-lower(name) REGEXP '[[:space:]]*bus  stop'
+lower(name) like '%  bus  stop%'
+;
+
+select
+stop_id
+,name
+/*,substr(name from 1 for instr(lower(name), ' bus stop')-1) as op1*/
+/*,substr(name from 1 for instr(lower(name), '  bus  stop')-1) as op2*/
+from stop
+WHERE 
+lower(name) REGEXP '[[:space:]]*bus[[:space:]]*stop'
+/*and stop_id=1230*/
 ;
