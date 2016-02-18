0735b3728ad3c7d4db705b7a00060ced100bf441
Cloud User <ec2-user@ip-172-31-20-22.ap-southeast-1.compute.internal>
2016-02-18 10:38:20 +0530

    identify changed segments after stops are relocated

---
+++ code/scripts/add_seg_pk.sql
@@ -0,0 +1,6 @@
+alter table segment add column is_stale boolean default 0; 
+
+SET FOREIGN_KEY_CHECKS = 0;
+alter table segment add primary key (from_stop_id, to_stop_id);
+
+SET FOREIGN_KEY_CHECKS = 0;
