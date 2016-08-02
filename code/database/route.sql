cea2cc71b86cc8b688f8fcc3da898694c11dffe2
Cloud User <ec2-user@ip-172-31-31-88.ap-southeast-1.compute.internal>
2016-08-02 17:12:46 +0530

    Make trip dirty if autocompleted

--- code/database/route.sql
+++ code/database/route.sql
@@ -1,7 +1,7 @@
 SELECT distinct R.route_id
 ,F.gtfs_agency_id as agency_id
 ,'' as route_short_name
-,CONCAT('"',concat(replace(replace(S1.name,'KTC Bus Stand',''), ' Ferry Terminal',''), case F.fleet_type when 4 then ' - ' else ' to ' end, replace(replace(S2.name,'KTC Bus Stand',''),' Ferry Terminal','')),'"')  as route_long_name
+,CONCAT('"',concat(replace(replace(S1.name,' KTC Bus Stand',''), ' Ferry Terminal',''), case F.fleet_type when 4 then ' - ' else ' to ' end, replace(replace(S2.name,' KTC Bus Stand',''),' Ferry Terminal','')),'"')  as route_long_name
 ,F.fleet_type as route_type
 from route R
 inner join stop S1 on S1.stop_id=R.start_stop_id
