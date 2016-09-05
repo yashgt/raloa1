d8d5ffb7b4a6e972b8c065fe80d2b2d8e9b28852
Cloud User <ec2-user@ip-172-31-31-88.ap-southeast-1.compute.internal>
2016-09-05 05:59:56 +0530

    remove dups

--- code/scripts/dup_routestops.sql
+++ code/scripts/dup_routestops.sql
@@ -8,6 +8,7 @@ group by stage_id, stop_id, peer_stop_id
 order by min(sequence)
 ) T2 on (T1.stage_id= T2.stage_id and T1.stop_id=T2.stop_id and T1.peer_stop_id=T2.peer_stop_id and T1.sequence<>T2.minseq)
 where T1.route_id=185
+;
 
 
 delete T1
@@ -20,3 +21,4 @@ group by stage_id, stop_id, peer_stop_id
 order by min(sequence)
 ) T2 on (T1.stage_id= T2.stage_id and T1.stop_id=T2.stop_id and T1.peer_stop_id=T2.peer_stop_id and T1.route_stop_id<>T2.minrsid)
 where T1.route_id=185
+;
