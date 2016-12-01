74cfd93047cddca012363c92621d4645753e7be3
Cloud User <yashgt@gmail.com>
2016-12-01 12:44:17 +0530

    incorrect route sequence fixed

--- code/database/procs.sql
+++ code/database/procs.sql
@@ -332,7 +332,7 @@ begin
 				from stage SG 
 				where SG.route_id=R.route_id 
 				and SG.stage_id=(select max(SG1.stage_id) from stage SG1 where SG1.route_id=R.route_id group by SG1.route_id))) as end_stop_name
-, (select case count(*) when 0 then 0 else 1 end from trip where route_id=R.route_id and fleet_id=7 limit 1) * 8
+, (select case count(*) when 0 then 0 else 1 end from trip where route_id=R.route_id and fleet_id=in_fleet_id limit 1) * 8
 | (select case count(*) when 0 then 0 else 1 end from internal_route_map where route_id=R.route_id limit 1) * 4 
 | (select case count(*) when 0 then 0 else 1 end from stage SG where SG.route_id=R.route_id limit 1) * 2 
 | (select case count(*) when 0 then 0 else 1 end from routestop RS where RS.route_id=R.route_id limit 1)
@@ -536,7 +536,8 @@ begin
 	left outer join segment FS on (FS.from_stop_id=NRS.peer_stop_id and FS.to_stop_id=PS.stop_id) 	
 	
 	where R.route_id=in_route_id
-	order by coalesce(SG.stage_id,0)*1000 + coalesce(RS.sequence, 0);
+	/*order by coalesce(SG.stage_id,0)*1000 + coalesce(RS.sequence, 0);*/
+        order by coalesce(RS.sequence,SG.stage_id*1000);
 	
 	select T.trip_id
 	, T.fleet_id as fleet_id
