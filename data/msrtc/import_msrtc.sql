e66555a45aa40cdfd007178e6862aa4c46859a93
Cloud User <yashgt@gmail.com>
2017-03-12 09:48:48 +0530

    import msrtc

--- data/msrtc/import_msrtc.sql
+++ data/msrtc/import_msrtc.sql
@@ -13,41 +13,45 @@ begin
 	declare c_stops cursor for 
 	select S1.bus_stop_cd, S1.bus_stop_nm 
 	from msrtc1.listofstops S1
-	/*left outer join stop S2 use index (idx_stop_code) on (S2.code=S1.bus_stop_cd and S2.fleet_id=7)
-	where S2.stop_id is null */   	
+	/*import ones that are not already present */
+	left outer join stop S2 use index (idx_stop_code) on (S2.code=S1.bus_stop_cd and S2.fleet_id=7)
+	where S2.stop_id is null   	
 	;
+
 	declare c_routes cursor for
 	select 
 	R1.route_no, R1.route_name
 	,S1.stop_id, S2.stop_id
 	from msrtc1.listofroutes R1 
-	inner join stop S1 on (S1.code=R1.from_stop_cd and S1.fleet_id=7)
+	inner join stop S1 on (S1.code=R1.from_stop_cd and S1.fleet_id=7) /*import only those whose start and end stops are present already */
 	inner join stop S2 on (S2.code=R1.till_stop_cd and S2.fleet_id=7)
+/* We will have one TARA route per ETM route and hence the below condition is commented 
 	left outer join msrtc1.listofroutes R2 on
 	(
 	R2.from_stop_cd=R1.till_stop_cd
         and R2.till_stop_cd=R1.from_stop_cd
-        /*and ((R2.via_stop_cd=R1.via_stop_cd) or (R2.via_stop_cd is null and R1.via_stop_cd is null))*/
 		and case instr(R1.route_name, ' via')
 			when 0 then instr(R2.route_name, ' via')=0
 			else trim(substr(R1.route_name from instr(R1.route_name, ' via') + 5))=trim(substr(R2.route_name from instr(R2.route_name, ' via') + 5))
 		end        
         )
 	where R1.from_stop_cd<=R1.till_stop_cd
+*/
 	and exists (select 1 from msrtc1.listofstopsonroutes SOR where SOR.route_no=R1.route_no)
-	/*left outer join route R on (LOR.route_no=R.route_cd and R.fleet_id=7)
-	left outer join internal_route_map M on (M.route_id=R.route_id and LOR.route_no=M.internal_route_cd)
-	where R.route_id is null*/
+	left outer join route R on ( R.fleet_id=7)
+	left outer join internal_route_map M on (M.route_id=R.route_id and R1.route_no=M.internal_route_cd)
+	where R.route_id is null
 	limit 500
 	;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
+/*
 	delete 
-	/* select * */
 	from internal_route_map 
 	where route_id in (select route_id from route where fleet_id=7);
 	delete from route where fleet_id=7;
     	delete from stop where fleet_id=7;
+*/
 
 
     open c_stops;
