78747811aa4910baaa6bdae58807da6f16f930ec
Cloud User <yashgt@gmail.com>
2017-03-21 13:42:44 +0530

    changes

--- data/msrtc/data/data/latlong/import_latlong.sh
+++ data/msrtc/data/data/latlong/import_latlong.sh
@@ -6,14 +6,16 @@ for i in *.xlsx
 do
 	j=`echo $i | cut -d'.' -f1`
 	echo "Importing from ${j}"
-	xlsx2csv -s 1 "${i}" | sed -n -e '/,[A-Z]\+$/p' | grep -i "^[^,]*,${j}" | grep -i "Bus terminus" | sed "s/^\([^,]*,\)\{2\}\([^,]*\),[^,]*,\([^,]*\),\([^,]*\),.*,\([A-Z]\+\)$/set @id=0; call save_stop(@id, '\2', '\5', \3,\4, 7, null, 3);/" >> latlong.sql
+	echo "/* Stops provided by ${j} */" >> latlong.sql
+	xlsx2csv -s 1 "${i}" | sed -n -e '/,[A-Z]\+,*$/p' | grep -i "^[^,]*,${j}" | grep -i "Bus terminus" | sed "s/^\([^,]*,\)\{2\}\([^,]*\),[^,]*,\([^,]*\),\([^,]*\),.*,\([A-Z]\+\),*$/set @id=0; call save_stop(@id, '\2', '\5', \3,\4, 7, null, 3);/" >> latlong.sql
 done
 
 for i in *.xls
 do
 	j=`echo $i | cut -d'.' -f1`
 	echo "Importing from ${j}"
+	echo "/* Stops provided by ${j} */" >> latlong.sql
 	xls2csv -x "${i}" -c a.csv 
-	cat a.csv | sed -n -e '/,[A-Z]\+$/p' | grep -i "^[^,]*,${j}" | grep -i "Bus terminus" | sed "s/\([^,]*,\)\{2\}\"\{0,1\}\([^,\"]*\)\"\{0,1\},[^,]*,\([^,]*\),\([^,]*\),.*,\([A-Z]\+\)$/set @id=0; call save_stop(@id, '\2', '\5', \3,\4, 7, null, 3);/" >> latlong.sql
+	cat a.csv | sed -n -e '/,[A-Z]\+,*$/p' | grep -i "^[^,]*,${j}" | grep -i "Bus terminus" | sed "s/\([^,]*,\)\{2\}\"\{0,1\}\([^,\"]*\)\"\{0,1\},[^,]*,\([^,]*\),\([^,]*\),.*,\([A-Z]\+\),*$/set @id=0; call save_stop(@id, '\2', '\5', \3,\4, 7, null, 3);/" >> latlong.sql
 done
 
