79e3308616e7baeced6d3311b4ec6deff20eb0df
Yash Ganthe <yash_ganthe@persistent.com>
2023-09-21 18:29:10 +0530

    restoreprod deleted

--- code/database/restoreprod.bat
+++
@@ -1,3 +0,0 @@
-mysqldump --ignore-table=raloa2.sessions  --host=taradb.cnld6dhtkcap.ap-south-1.rds.amazonaws.com --user=root --password=Spider123 --add-drop-database --databases raloa2 > proddump.sql
-mysql --host=localhost --user=root --password=Spider123 < proddump.sql
-mysql --host=localhost --user=root --password=Spider123 --database=raloa2 < procs.sql
