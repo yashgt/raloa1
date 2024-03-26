1c10cf1d02b62084629a6bc9e3b51f3ded6c70b6
Yash Ganthe <yashgt@gmail.com>
2024-03-26 10:01:11 +0530

    geocoding complete

---
+++ code/scripts/validategtfs.bat
@@ -0,0 +1,7 @@
+set basedir=C:\mydata\Projects\NewYug\raloa1\data\ktc\feed
+cd %basedir%
+REM more +1 fare_rules_2.txt >> fare_rules.txt
+tail -n +2 fare_rules_2.txt >> fare_rules.txt
+del fare_rules_2.txt
+tar -acf gtfs.zip *.txt
+java -jar C:\mydata\UTILS\gtfs-validator-4.2.0-cli.jar -i %basedir%\gtfs.zip -o %HOMEPATH%
